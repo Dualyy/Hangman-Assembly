78a44ca6337de5edf9642710a43817854a8c8057
Lars Frassle <l.fraessle1206@protonmail.com>
2025-01-14 11:42:26 +0000

    add comments to Letterbox component for better clarity and documentation

--- Hangman-Assembly/src/letterbox.jsx
+++ Hangman-Assembly/src/letterbox.jsx
@@ -1,11 +1,11 @@
 import PropTypes from 'prop-types';
 
 
-
+// Letterbox component is a functional component that takes in the following props:
 export default function Letterbox({ letter, isClicked, changeClick, id, word, wrongClick }) {
     const styleCorrect = { backgroundColor: "green"};
     const styleWrong = { backgroundColor: "red" };
-
+    // boxStyle function is used to change the color of the letter box based on the user's input and wether it is right or wrong.
     const boxStyle = () => {
         if (isClicked) {
             if (word.includes(letter)) {
@@ -25,7 +25,7 @@ export default function Letterbox({ letter, isClicked, changeClick, id, word, wr
     
     
 }
-
+// PropTypes is a library that helps in validating the props passed to a component. It also helps in documenting the intended types of properties passed to components.
 Letterbox.propTypes = {
     letter: PropTypes.string.isRequired,
     isClicked: PropTypes.bool.isRequired,
